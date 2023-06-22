a1f05d8f32cc337808973ef797ed2be15f4f7197
caitlynjincu <cj384@cornell.edu>
2023-06-22 16:45:59 -0700

    Initialize app with service account key

--- functions/index.js
+++ functions/index.js
@@ -7,23 +7,15 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-// const { onRequest } = require("firebase-functions/v2/https");
-// const logger = require("firebase-functions/logger");
-
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", { structuredData: true });
-//   response.send("Hello from Firebase!");
-// });
 
 const functions = require("firebase-functions");
 const express = require("express");
 const app = express();
 
-const admin = require("firebase-admin/app");
-const serviceAccount = require("../service_account_key.json");
+const admin = require("firebase-admin");
+const serviceAccount = "./service_account_key.json";
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
