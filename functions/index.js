f5c2e430762e6f0d4d4cbab6594c07f94772506e
Vin Bui <vdb23@cornell.edu>
2023-07-10 22:41:09 -0500

    Added creating post

--- functions/index.js
+++ functions/index.js
@@ -17,6 +17,7 @@ app.use(express.json());
 
 // Constants
 const db = getFirestore();
+const { v4: uuidv4 } = require("uuid");
 
 // ======= Routes =======
 
@@ -31,14 +32,47 @@ app.get("/", (req, res) => {
  * This route returns all posts
  */
 app.get("/api/posts/", async (req, res) => {
-  const snapshot = await db.collection("posts").get();
+  try {
+    const snapshot = await db.collection("posts").get();
 
-  let posts = [];
-  snapshot.forEach((doc) => {
-    posts.push(doc.data());
-  });
+    let posts = [];
+    snapshot.forEach((doc) => {
+      posts.push(doc.data());
+    });
 
-  return res.status(200).json(posts);
+    return res.status(200).json(posts);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+});
+
+/**
+ * This route adds a new post to the database given a message
+ *
+ * - message (String): the message of the post
+ */
+app.post("/api/posts/create/", async (req, res) => {
+  const message = req.body.message;
+  if (message == null) {
+    return res
+      .status(400)
+      .send("Missing parameter `message` from request body");
+  }
+
+  const id = uuidv4();
+  const data = {
+    id: id,
+    likes: [],
+    message: message,
+    time: new Date().toISOString(),
+  };
+
+  try {
+    await db.collection("posts").doc(id).set(data);
+    return res.status(201).json(data);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
 });
 
 exports.chatdev = onRequest({ region: "us-east1" }, app);
